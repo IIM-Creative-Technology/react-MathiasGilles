8da2e544666767308fff71e06ceadcdd05a86b6e
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-03 15:03:40 +0100

    [update] name on the input

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,7 +5,7 @@ import '../scss/components/_inputRadio.scss'
 const Input = (props) => {
     return (
         <div className="container-input-radio">
-            <input className="input" type="radio"/>
+            <input name="input" className="input" type="radio"/>
             <p className="answer">{props.question}</p>
         </div>
     )
