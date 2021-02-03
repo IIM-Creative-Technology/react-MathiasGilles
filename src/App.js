0f141001f7fbe37eeeeed8d1ca8465f1356b1a58
MathiasGilles <mathiasgilles136@gmail.com>
2021-02-03 16:03:53 +0100

    [add] React router and sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import Questionnaire from "./components/Questionnaire";
-import Question from "./data/questions.json"
 
 
 function App() {
-    console.log(Question.All)
   return (
     <div className="App">
       <Questionnaire/>
