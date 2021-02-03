e3c9a0b7dc9bee37eb4819118ce449007f0f4d0e
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-02-03 14:59:39 +0100

    [add] for boucle to show all question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
 import "../scss/layout/_question.scss"
 import Input from "./Input";
 
+const question = {
+    'question': "Voici la question",
+    'inputs': [
+        "je suis le input 1",
+        "je suis le input 2",
+        "je suis le input 3",
+        "je suis le input 4"
+    ]
+}
+
+
 const Question = () => {
     return (
         <div className="container-question">
             <h2>Ceci est un test zeubi</h2>
-            <Input question="Voici la question"></Input>
+            <>{GetAllInputs}</>
         </div>
     );
 }
+
+const GetAllInputs = question.inputs.map( (input) => <Input question={input}></Input>);
+
 
 export default Question
