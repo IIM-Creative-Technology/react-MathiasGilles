0ace55bb3229dd793e788024c252ebf94971edae
MathiasGilles <mathiasgilles136@gmail.com>
2021-02-05 09:40:57 +0100

    [add] presentation component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import NavBarre from "./Nav";
 import Questionnaire from "./Questionnaire";
+import Presentation from "./Presentation";
 
 function HomePage(){
     return(
         <main>
             <NavBarre/>
+            <Presentation/>
             <Questionnaire/>
         </main>
     )
