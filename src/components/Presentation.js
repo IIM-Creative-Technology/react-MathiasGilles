479d27997690b9cb9b00566bdc943dfde5de103f
MathiasGilles <mathiasgilles136@gmail.com>
2021-02-05 09:45:47 +0100

    [add] presentation html

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,5 +1,18 @@
-function Presentation(){
+import "../scss/layout/_presentation_home.scss"
 
+function Presentation(){
+    return(
+        <section className="presentation-container">
+            <article>
+                <h1>Des Quizz incroyables</h1>
+                <p>inspiré par Yanis Elmaleh</p>
+                <button>Voir quizz</button>
+            </article>
+            <article>
+                <img src="http://place-hold.it/400x400" alt=""/>
+            </article>
+        </section>
+    )
 }
 
 export default Presentation;
